d1dd26ab9763ddcf565e93751dd01fcba6140ba5
nehul vasava <nehulvasava15@gmail.com>
2024-04-01 13:34:21 +0530

    sss

--- index.js
+++ index.js
@@ -258,7 +258,7 @@ app.post("/unbookmark/:postId", async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+// app.use(express.static(path.join(__dirname, "/client/dist")));
 
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
